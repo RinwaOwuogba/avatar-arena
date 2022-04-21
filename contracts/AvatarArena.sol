c4393e82cbb850cd5b67658a71208b542fb67511
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-04-21 22:28:35 +0100

    test: added tests for AvatarArena contract. ch: updated deploy script for AvatarArena contract.

---
+++ contracts/AvatarArena.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+abstract contract Arena is ERC721 {
+    struct Player {
+        address player;
+        uint256 nft;
+    }
+    struct Battle {
+        Player[] players;
+        uint256 createdAt;
+        int256 winner;
+    }
+
+    event BattleComplete(uint256 battleIndex);
+    Battle[] private _battles;
+
+    mapping(address => uint256) private _userBattles;
+    mapping(uint256 => uint256) private _avatarWins;
+
+    constructor() {}
+
+    /**
+    Creates a new battle for the sender or adds sender
+    to a pending battle
+     */
+    function startBattle(uint256 tokenId) external {
+        require(
+            this.ownerOf(tokenId) == msg.sender,
+            "Arena: Cannot start battle with non-owned token"
+        );
+
+        if (_battles.length > 0) {
+            uint256 currentBattleIndex = _battles.length - 1;
+            Battle storage currentBattle = _battles[currentBattleIndex];
+
+            // try to join an existing battle
+            if (currentBattle.players.length == 1) {
+                currentBattle.players.push(Player(msg.sender, tokenId));
+                _userBattles[msg.sender] = currentBattleIndex;
+
+                _simulateBattle(currentBattleIndex);
+                return;
+            }
+        }
+
+        Battle storage newBattle = _battles.push();
+
+        newBattle.players.push(Player(msg.sender, tokenId));
+        newBattle.createdAt = block.timestamp;
+        newBattle.winner = -1;
+
+        _userBattles[msg.sender] = _battles.length - 1;
+    }
+
+    /**
+    Get sender's current battle
+     */
+    function getBattle() external view returns (Battle memory) {
+        uint256 battleIndex = _userBattles[msg.sender];
+
+        return (_battles[battleIndex]);
+    }
+
+    /**
+    Returns no of wins an avatar has
+    */
+    function getAvatarWins(uint256 tokenId) external view returns (uint256) {
+        return _avatarWins[tokenId];
+    }
+
+    /**
+    Get the winner of a battle
+     */
+    function _simulateBattle(uint256 _battleIndex) internal {
+        uint256 winnerIndex = uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender))
+        ) % 2;
+
+        Battle storage battle = _battles[_battleIndex];
+        battle.winner = int256(winnerIndex);
+        uint256 winningNft = battle.players[winnerIndex].nft;
+
+        _avatarWins[winningNft] += 1;
+
+        emit BattleComplete(_battleIndex);
+    }
+}
+
+contract AvatarArena is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable,
+    Arena
+{
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("AvatarArena", "AVAR") {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/avatar-arena-test.js
@@ -0,0 +1,115 @@
+// const { default: BigNumber } = require("bignumber.js");
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+
+describe("AvatarArena", function () {
+  this.timeout(50000); // line 5
+
+  let avatarArena;
+  let owner;
+  let acc1;
+  let acc2;
+
+  this.beforeEach(async function () {
+    const AvatarArena = await ethers.getContractFactory("AvatarArena");
+    [owner, acc1, acc2] = await ethers.getSigners();
+
+    avatarArena = await AvatarArena.deploy();
+  });
+
+  it("should set the right owner", async function () {
+    expect(await avatarArena.owner()).to.equal(owner.address);
+  });
+
+  it("should mint one NFT", async function () {
+    expect(await avatarArena.balanceOf(acc1.address)).to.equal(0);
+
+    const tokenURI = "https://example.com/1";
+
+    await mintNFT(avatarArena, owner, acc1.address, tokenURI);
+
+    expect(await avatarArena.balanceOf(acc1.address)).to.equal(1);
+  });
+
+  it("should set the correct tokenURI", async function () {
+    const tokenURI_1 = "https://example.com/1";
+    const tokenURI_2 = "https://example.com/2";
+
+    await mintNFT(avatarArena, owner, acc1.address, tokenURI_1);
+    await mintNFT(avatarArena, owner, acc2.address, tokenURI_2);
+
+    expect(await avatarArena.tokenURI(0)).to.equal(tokenURI_1);
+    expect(await avatarArena.tokenURI(1)).to.equal(tokenURI_2);
+  });
+
+  describe("startBattle", () => {
+    it("should start a pending battle if no pending battle is available", async () => {
+      await mintNFT(avatarArena, owner, owner.address);
+
+      const tokenID = 0;
+      const trx = await avatarArena.connect(owner).startBattle(tokenID);
+      await trx.wait();
+
+      const battle = await avatarArena.connect(owner).getBattle();
+
+      expect(battle.players[0].player).to.eql(owner.address);
+      expect(battle.players[0].nft).to.eq(BigNumber.from(tokenID));
+
+      // no winner should exist until game is completed
+      expect(battle.winner).to.eq(-1);
+    });
+
+    it("should join latest pending battle and complete it", async () => {
+      await mintNFT(avatarArena, owner, owner.address);
+      await mintNFT(avatarArena, acc1, acc1.address);
+
+      const tokenID_1 = 0;
+      const tokenID_2 = 1;
+
+      // NFT Avatars should start with zero wins
+      const wins_1 = await avatarArena.connect(owner).getAvatarWins(tokenID_1);
+      const wins_2 = await avatarArena.connect(owner).getAvatarWins(tokenID_1);
+
+      expect(wins_1).to.eq(0);
+      expect(wins_2).to.eq(0);
+
+      const battleId = 0;
+      await avatarArena.connect(owner).startBattle(tokenID_1);
+      await expect(avatarArena.connect(acc1).startBattle(tokenID_2))
+        .to.emit(avatarArena, "BattleComplete")
+        .withArgs(battleId);
+
+      const battle = await avatarArena.connect(acc1).getBattle();
+
+      expect(battle.players[0].player).to.eql(owner.address);
+      expect(battle.players[1].player).to.eql(acc1.address);
+      expect(BigNumber.from(battle.winner).toNumber()).to.be.oneOf([0, 1]);
+
+      const winningNftId = battle.players[battle.winner].nft;
+      expect(
+        await avatarArena.connect(owner).getAvatarWins(winningNftId)
+      ).to.eq(1);
+    });
+
+    it("should fail to start a battle with a token sender does not own", async () => {
+      await mintNFT(avatarArena, owner, acc1.address);
+
+      const tokenID_1 = 0;
+
+      await expect(
+        avatarArena.connect(owner).startBattle(tokenID_1)
+      ).to.be.revertedWith("Arena: Cannot start battle with non-owned token");
+    });
+  });
+});
+
+const mintNFT = async (
+  contractInstance,
+  as,
+  to,
+  tokenURI = "https://example.com/1"
+) => {
+  const trx1 = await contractInstance.connect(as).safeMint(to, tokenURI);
+  return trx1.wait();
+};
